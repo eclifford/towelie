84f06eb8a61d2253f1c5e64143daea650f037376
Eric Clifford <ericgclifford@gmail.com>
2012-10-29 16:31:34 -0700

    adding new files

---
+++ tasks/server.js
@@ -0,0 +1,59 @@
+var connect = require('connect')
+    path    = require('path')
+    open    = require('open')
+
+module.exports = function(grunt) {
+  grunt.renameTask('server', 'grunt-server');
+
+  grunt.registerTask('server', 'Launch a preview server', function() { 
+   // Merge task-specific options with these defaults.
+      // var options = this.data.options({
+      //   port: 8000,
+      //   hostname: 'localhost',
+      //   base: '.',
+      //   keepalive: true
+      // });
+
+      // Connect requires the base path to be absolute.
+      var base = path.resolve('app');
+
+      // Sweet, sweet middleware.
+      var middleware = [
+        // Serve static files.
+        connect.static('app'),
+        connect.static('temp'),
+        // Make empty directories browsable. (overkill?)
+        connect.directory('app')
+      ];
+
+      // If --debug was specified, enable logging.
+      if (grunt.option('debug')) {
+        connect.logger.format('grunt', ('[D] server :method :url :status ' +
+          ':res[content-length] - :response-time ms').magenta);
+        middleware.unshift(connect.logger('grunt'));
+      }
+
+      // Start server.
+      grunt.log.writeln('Starting static web server on ' + 'localhost' + ':' + '8000' + '.');
+      connect.apply(null, middleware).listen('8000', 'localhost');
+
+      open( 'http://' + 'localhost' + ':' + '8000' );
+        // if ( opts.open ) {
+          
+        // }
+
+      // // So many people expect this task to keep alive that I'm adding an option
+      // // for it. Running the task explicitly as grunt:keepalive will override any
+      // // value stored in the config. Have fun, people.
+      // if (this.flags.keepalive || true) {
+      //   // This is now an async task. Since we don't store a handle to the "done"
+      //   // function, this task will never, ever, ever terminate. Have fun!
+      //   this.async();
+      //   grunt.log.write('Waiting forever...');
+      // }
+  });
+
+  // grunt.registerTask('open-browser', function() {
+  //   open('http://' + grunt.config('server.hostname'))
+  // });
+}
---
+++ tasks/towelie.js
@@ -0,0 +1,18 @@
+var fs    = require('fs');
+var join  = require('path').join;
+
+module.exports = function(grunt) {
+  var grunt_plugins = [
+    "grunt-contrib/node_modules/grunt-contrib-coffee",
+    "grunt-contrib/node_modules/grunt-contrib-clean",
+    "grunt-jam",
+    "grunt-requirejs",
+    "grunt-testem",
+    "grunt-sass"
+  ];
+
+   // Let's load all the pass through tasks 
+  for (var i = 0; i < grunt_plugins.length; i++) {
+    grunt.loadTasks(join(__dirname, '../node_modules/' + grunt_plugins[i] + '/tasks'));
+  }
+}
