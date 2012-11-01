471be01c340d7d1a573bea507f13939c8ca96fb8
Eric Clifford <ericgclifford@gmail.com>
2012-11-01 15:37:53 -0700

    fixed duplicate compass tasks firing

--- tasks/towelie.js
+++ tasks/towelie.js
@@ -6,14 +6,13 @@ module.exports = function(grunt) {
     "grunt-contrib/node_modules/grunt-contrib-coffee",
     "grunt-contrib/node_modules/grunt-contrib-clean",
     "grunt-contrib/node_modules/grunt-contrib-copy",
-    "grunt-jam",
-    "grunt-requirejs",
+    "grunt-contrib/node_modules/grunt-contrib-requirejs",
     "grunt-testem",
     "grunt-sass",
     "grunt-compass"
   ];
 
-   // Let's load all the pass through tasks 
+  // Let's load all the pass through tasks 
   for (var i = 0; i < grunt_plugins.length; i++) {
     grunt.loadTasks(join(__dirname, '../node_modules/' + grunt_plugins[i] + '/tasks'));
   }
